541bff03bbba48f9d9b26a74473e743aed1086ff
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-06-24 18:18:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Router>
     <Routes>
-      <Route path='https://brijesh1990.github.io/' element={<Layout />}></Route>
+      <Route path='https://brijesh1990.github.io/portfolio-app/' element={<Layout />}></Route>
       <Route path='/contact-us' element={<Contact />}></Route>
       <Route path='/delete-contact/:id' element={<DeleteContact />}></Route>
       <Route path='/edit-contact/:id' element={<EditContact />}></Route>

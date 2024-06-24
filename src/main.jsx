516b4db5f9fc2199a7c1d56b8792655e1ee8b3c1
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-06-24 17:59:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Router>
     <Routes>
-      <Route path='/' element={<Layout />}></Route>
-      <Route path='/contact-us' element={<Contact />}></Route>
-      <Route path='/delete-contact/:id' element={<DeleteContact />}></Route>
-      <Route path='/edit-contact/:id' element={<EditContact />}></Route>
+      <Route path='/portfolio-app/' element={<Layout />}></Route>
+      <Route path='/portfolio-app/contact-us' element={<Contact />}></Route>
+      <Route path='/portfolio-app/delete-contact/:id' element={<DeleteContact />}></Route>
+      <Route path='/portfolio-app/edit-contact/:id' element={<EditContact />}></Route>
     </Routes>
    </Router>
   </React.StrictMode>,
